d0cfaf90db6e66ee2dc63966ffba20087ecdf2bf
ron rock <ronrk11@gmail.com>
2022-01-09 12:51:39 +0200

    bugs with user routes

--- models/user.js
+++ models/user.js
@@ -2,30 +2,33 @@ const { Model, DataTypes } = require("sequelize");
 const { sequelize } = require(".");
 
 module.exports = (sequelize) => {
-    class User extends Model {}
-    User.init({
-        firstName: {
-            type: DataTypes.STRING,
-        },
-        lastName: {
-            type: DataTypes.STRING,
-        },
-        emailAddress: {
-            type: DataTypes.STRING,
-        },
-        password: {
-            type: DataTypes.STRING,
-        },
-    }, { sequelize });
+  class User extends Model {}
+  User.init(
+    {
+      firstName: {
+        type: DataTypes.STRING,
+      },
+      lastName: {
+        type: DataTypes.STRING,
+      },
+      emailAddress: {
+        type: DataTypes.STRING,
+      },
+      password: {
+        type: DataTypes.STRING,
+      },
+    },
+    { sequelize }
+  );
 
-    User.associate = (models) => {
-        User.hasMany(models.Course, {
-            as: "Student",
-            foreignKey: {
-                fieldName: "userId",
-                allowNull: false,
-            },
-        });
-    };
-    return User;
+  User.associate = (models) => {
+    User.hasMany(models.Course, {
+      as: "Student",
+      foreignKey: {
+        fieldName: "userId",
+        allowNull: false,
+      },
+    });
+  };
+  return User;
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,27 +6,33 @@ const { User } = require("../models");
 const router = express.Router();
 
 router.get(
-    "/",
-    asyncHandler(async(req, res) => {
-        const user = await User.findAll();
-        res.json({
-            user,
-        });
-    })
+  "/",
+  asyncHandler(async (req, res) => {
+    const users = await User.findAll();
+    res.json({
+      users,
+    });
+  })
 );
 
-router.post('/', asyncHandler(async(req, res) => {
+router.post(
+  "/",
+  asyncHandler(async (req, res) => {
     try {
-        await User.create(req.body);
-        res.status(201).json({ "message": "Account successfully created!" });
+      await User.create(req.body);
+      res.status(201).json({ message: "Account successfully created!" });
     } catch (error) {
-        if (error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') {
-            const errors = error.errors.map(err => err.message);
-            res.status(400).json({ errors });
-        } else {
-            throw error;
-        }
+      if (
+        error.name === "SequelizeValidationError" ||
+        error.name === "SequelizeUniqueConstraintError"
+      ) {
+        const errors = error.errors.map((err) => err.message);
+        res.status(400).json({ errors });
+      } else {
+        throw error;
+      }
     }
-}));
+  })
+);
 
 module.exports = router;
